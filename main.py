89e8caad5bad797bd99668690811e83772e894a5
David Wallace <David@David-Macbook-Air.local>
2023-01-29 15:36:08 -0600

    refreshed gitignore

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import config
-import time
+import config, requests, json
 import tweepy
-from tweepy.api import pagination
-from tweepy.client import Response
-import csv
 from datetime import date
 
-# date
-current_date = str(date.today())
-
-
-client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
 auth = tweepy.OAuthHandler(consumer_key=config.API_KEY,
                            consumer_secret=config.API_KEY_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+
+data = []
+
+api.update_status("Food processor on the way")
+
+def get_food_data():
+    global data 
+    res = requests.get('https://data.cityofchicago.org/resource/4ijn-s7e5.json')
+    data = res.text
+    print(len(data), " results" )
+
+get_food_data()
+
+# def get_food_fails():
---
+++ readme.md
@@ -0,0 +1,10 @@
+Generate keys from Twitter Dev Portal and the associated account. Insert keys into the config file:
+
+```
+API_KEY=c
+API_KEY_SECRET=c
+BEARER_TOKEN=c
+ACCESS_TOKEN=c
+ACCESS_TOKEN_SECRET=c
+
+```
